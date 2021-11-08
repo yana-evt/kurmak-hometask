7b87ae3029ab6ab2afd6ab0362c3c4626e3d03bc
Iana Evtukhova <iana.evtukhova@team.wrike.com>
2021-11-08 18:56:47 +0100

    tabs

--- script.js
+++ script.js
@@ -30,7 +30,7 @@ prev.addEventListener('click', e => {
   prevSlide();
 } )
 
-////
+// Dialog
 const KEYCODE= {
   ESC: 27,
 };
@@ -92,4 +92,23 @@ function checkCloseDialog(event) {
   if (event.keyCode === KEYCODE.ESC) {
     closeDialog();
   }
+}
+
+// Tabs
+
+function openTab(evt, tabName) {
+  var i, tabcontent, tablinks;
+
+  tabcontent = document.getElementsByClassName("tabcontent");
+  for (i = 0; i < tabcontent.length; i++) {
+    tabcontent[i].style.display = "none";
+  }
+
+  tablinks = document.getElementsByClassName("tablinks");
+  for (i = 0; i < tablinks.length; i++) {
+    tablinks[i].className = tablinks[i].className.replace(" about-museum__tab--active", "");
+  }
+
+  document.getElementById(tabName).style.display = "block";
+  evt.currentTarget.className += " about-museum__tab--active";
 }
