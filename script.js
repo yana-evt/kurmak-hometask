ba0d826259f8c3b8480c5d4e1931b7a14f2eea59
Iana Evtukhova <iana.evtukhova@team.wrike.com>
2021-11-13 19:53:14 +0100

    fix

--- script.js
+++ script.js
@@ -32,19 +32,17 @@ prev.addEventListener('click', e => {
 
 // Dialog
 const KEYCODE= {
+  ENTER: 13,
   ESC: 27,
 };
 
+const buttonLoginOpen = document.querySelector('.button-login-open');
 const dialog = document.querySelector('.modal-login');
 const dialogMask = document.querySelector('.modal-login__mask');
-
-let previouslyActiveElement;
 
 document.querySelector('.button-login-open').addEventListener('click', openDialog)
 
 function openDialog() {
-  previouslyActiveElement = document.activeElement;
-
   Array.from(document.body.children).forEach(child => {
     if (child !== dialog && child !== dialogMask) {
       child.inert = true;
@@ -57,24 +55,78 @@ function openDialog() {
   dialogMask.addEventListener('click', closeDialog);
   dialog.querySelector('.modal-login__enter').addEventListener('click', handleEnterButtonClick);
   dialog.querySelector('.modal-login__close').addEventListener('click', closeDialog);
+  dialog.querySelector('#userLogin').addEventListener('input', handleLoginInput);
+  dialog.querySelector('#userLogin').addEventListener('keydown', handleLoginPasswordKeydown);
+  dialog.querySelector('#userPassword').addEventListener('input', handlePasswordInput);
+  dialog.querySelector('#userPassword').addEventListener('keydown', handleLoginPasswordKeydown);
   document.addEventListener('keydown', checkCloseDialog);
 
   dialog.querySelector('.modal-login__input').focus();
 }
 
+function handleLoginInput() {
+  dialog.querySelector('#textInputLoginError').classList.remove('show');
+}
+
+function handlePasswordInput() {
+  dialog.querySelector('#textInputPasswordError').classList.remove('show');
+}
+
+function handleLoginPasswordKeydown(event) {
+  if (event.keyCode === KEYCODE.ENTER) {
+    Promise.resolve().then(handleLoginRequest)
+  }
+}
+
 function handleEnterButtonClick() {
+  Promise.resolve().then(handleLoginRequest)
+}
 
+function handleLoginRequest() {
+  let loginErrorMessage = dialog.querySelector('#textInputLoginError');
+  let passwordErrorMessage = dialog.querySelector('#textInputPasswordError');
 
-  alert('Logged in');
+  hideErrors();
 
-  closeDialog();
+  let loginInput = dialog.querySelector('#userLogin');
+  let passwordInput = dialog.querySelector('#userPassword');
+
+  let loginIsMissing = loginInput.value == null || loginInput.value === '';
+  let passwordIsMissing = passwordInput.value == null || passwordInput.value === '';
+
+  if (loginIsMissing) {
+    loginErrorMessage.classList.add('show');
+  }
+
+  if (passwordIsMissing) {
+    passwordErrorMessage.classList.add('show');
+  }
+
+  if (!loginIsMissing && !passwordIsMissing) {
+    alert('Logged in');
+
+    closeDialog();
+  }
+}
+
+function hideErrors() {
+  dialog.querySelector('#textInputLoginError').classList.remove('show');
+  dialog.querySelector('#textInputPasswordError').classList.remove('show');
 }
 
 function closeDialog() {
   dialogMask.removeEventListener('click', closeDialog);
   dialog.querySelector('.modal-login__enter').removeEventListener('click', handleEnterButtonClick);
   dialog.querySelector('.modal-login__close').removeEventListener('click', closeDialog);
+  dialog.querySelector('#userLogin').removeEventListener('input', handleLoginInput);
+  dialog.querySelector('#userLogin').removeEventListener('keydown', handleLoginPasswordKeydown);
+  dialog.querySelector('#userPassword').removeEventListener('input', handlePasswordInput);
+  dialog.querySelector('#userPassword').removeEventListener('keydown', handleLoginPasswordKeydown);
   document.removeEventListener('keydown', checkCloseDialog);
+
+  dialog.querySelector('#userLogin').value = '';
+  dialog.querySelector('#userPassword').value = '';
+  hideErrors();
 
   Array.from(document.body.children).forEach(child => {
     if (child !== dialog && child !== dialogMask) {
@@ -85,7 +137,7 @@ function closeDialog() {
   dialog.classList.remove('opened');
   dialogMask.classList.remove('opened');
 
-  previouslyActiveElement.focus();
+  buttonLoginOpen.focus();
 }
 
 function checkCloseDialog(event) {
